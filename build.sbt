b56691981a0b9b12b6589f27695e90db60f817de
Dani Sancas <daniel.sanchez.castello@everis.com>
2020-02-20 18:34:32 +0100

    Init project

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ name := "hashcode2020"
 version := "0.1"
 
 scalaVersion := "2.12.2"
-        
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.0" % "test"
---
+++ src/test/scala/com/google/hashcode/everisbioscala/HelloWorldTest.scala
@@ -0,0 +1,11 @@
+package com.google.hashcode.everisbioscala
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class HelloWorldTest {
+
+  "helloworld" should "say hi" in {
+    assert(true)
+  }
+
+}
