216ff1def6b02d2ec1e6d59a61a306994de577ef
Dani Sancas <daniel.sanchez.castello@everis.com>
2020-02-20 21:11:15 +0100

    Parser developed

--- src/main/scala/com/google/hashcode/everisbioscala/CaseClasses.scala
+++ src/main/scala/com/google/hashcode/everisbioscala/CaseClasses.scala
@@ -10,6 +10,6 @@ case class Library(books: List[Book], signupTime: Int, scanPerDay: Int)
 
 case class FirstLine(numBooks: Int, numLibraries: Int, numDays: Int)
 case class SecondLine(books: List[(Int, Int)])
-case class RestLines()
+case class RestLines(tuples: List[(String, Int, Int, List[Int])]) // List[numBooks: String, signupTime: Int, scanPerDay: Int, books: List[Int]]
 
 case class ReaderFirstPhase(numBooks: Int, numLibraries: Int, numDays: Int, libraries: List[Library])
--- src/main/scala/com/google/hashcode/everisbioscala/InputParser.scala
+++ src/main/scala/com/google/hashcode/everisbioscala/InputParser.scala
@@ -2,21 +2,19 @@ package com.google.hashcode.everisbioscala
 
 import scala.io.Source
 
-class InputParser {
+object InputParser {
   def readFile(path: String): ReaderFirstPhase  = {
     val input: List[String] = Source.fromFile(path).getLines().toList
     val firstLine = readFirstLine(input.head)
     val secondLine = readSecondLine(input.tail.head)
     val restLines = readRestLines(input.tail.tail)
-
-    ???
+    val merged: ReaderFirstPhase = mergeInfo(firstLine, secondLine, restLines)
+    merged
   }
 
   def readFirstLine(line: String): FirstLine = {
     val input = line.split(" ").toList.map(_.toInt)
-    val (numBooks, tail1) = (input.head, input.tail)
-    val (numLibraries, tail2) = (tail1.head, tail1.tail)
-    val numDays = tail2.head
+    val (numBooks, numLibraries, numDays) = (input(0), input(1), input(2))
     FirstLine(numBooks, numLibraries, numDays)
   }
 
@@ -26,5 +24,39 @@ class InputParser {
     SecondLine(indexed)
   }
 
-  def readRestLines(lines: List[String]) = ???
+  def readRestLines(lines: List[String]): RestLines = {
+
+    def helper(libraryInfo: String, libraryBooks: String): (String, Int, Int, List[Int]) = {
+      val infoSplit: List[String] = libraryInfo.split(" ").toList
+      val (numBooks: String, signupTime: Int, scanPerDay: Int) = (infoSplit(0), infoSplit(1).toInt, infoSplit(2).toInt)
+      val booksSplit: List[String] = libraryBooks.split(" ").toList
+      (numBooks, signupTime, scanPerDay, booksSplit.map(_.toInt))
+    }
+
+    val doubleList: Iterator[(String, String)] = for {
+      l <- lines.grouped(2)
+    } yield l.head -> l.tail.head
+
+    val result: List[(String, Int, Int, List[Int])]  = doubleList
+      .toList // List[(String, String)]
+      .map(x => helper(x._1, x._2))
+
+    RestLines(result)
+  }
+
+  def mergeInfo(firstLine: FirstLine, secondLine: SecondLine, restLines: RestLines): ReaderFirstPhase = {
+    val finalLibraryList: List[Library] = restLines
+      .tuples // List[numBooks: String, signupTime: Int, scanPerDay: Int, books: List[Int]]
+      .map{ x =>
+        val (numBooks: String, signupTime: Int, scanPerDay: Int, books: List[Int]) = x
+        val bookList: List[Book] = books
+        .map{ bookToFind: Int =>
+          val result: (Int, Int) = secondLine.books.find(_._1 == bookToFind).get
+          Book(result._1, result._2)
+        }
+        Library(bookList, signupTime, scanPerDay)
+      }
+
+    ReaderFirstPhase(firstLine.numBooks, firstLine.numLibraries, firstLine.numDays, finalLibraryList)
+  }
 }
